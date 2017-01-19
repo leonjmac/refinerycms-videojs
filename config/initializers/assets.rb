24626cba8c383cfdab48d694506cd0b3b9185cfd
Anita Graham <anita@joli.com.au>
2017-01-19 17:42:31 +0800

    Add assets to assets pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,9 @@
-Rails.application.config.assets.precompile  << /\.(?:svg)\z/
+Refinery::Core.configure do |config|
+  # Register extra javascript for backend
+  config.register_javascript "wymeditor_monkeypatch.js"
+
+  # Register extra stylesheet for backend (optional options)
+  config.register_stylesheet "refinerycms-videojs-admin.css",:media => 'screen'
+end
+Rails.application.config.assets.precompile += %w( refinerycms-videojs-admin.css wymeditor-monkeypatch.js )
+Rails.application.config.assets.precompile  += /\.(?:svg)\z/
