b0e9ed7f187677b7634afbdc6c7835a2227ddff6
Anita Graham <anita@joli.com.au>
2017-01-20 11:29:29 +0800

    Move assets around till the precompile works

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -1,8 +1,8 @@
 Refinery::Core.configure do |config|
   # Register extra javascript for backend
-  config.register_javascript "refinery/admin/refinerycms-videojs-admin.js"
+  config.register_javascript "refinerycms-videojs-admin"
 
   # Register extra stylesheet for backend (optional options)
-  config.register_stylesheet "refinery/admin/refinerycms-videojs-admin.css",:media => 'screen'
+  config.register_stylesheet "refinerycms-videojs-admin", :media => 'screen'
 end
 #  config/initializers/assets.rb adds these files to the asset pipeline
