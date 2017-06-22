5a4d5c831bf8d0d0995fd20fb87a4ecbcd4fb9aa
Anita Graham <anita@joli.com.au>
2017-06-22 13:28:18 +0800

    fix dialogs controller

--- app/decorators/controllers/refinery/dialogs_controller_decorator.rb
+++ app/decorators/controllers/refinery/dialogs_controller_decorator.rb
@@ -1,11 +1,12 @@
-Refinery::Admin::DialogsController.class_eval do
-  Refinery::Admin::DialogsController::TYPES << 'video'
-  def find_iframe_src_with_video
-    if @dialog_type == 'video'
-      @iframe_src = refinery.insert_videos_admin_videos_path url_params.merge(:dialog => true)
-    else
-      find_iframe_src_without_video
+Refinery::Admin::DialogsController::TYPES << 'video'
+Refinery::Admin::DialogsController.prepend(
+  Module.new do
+    def find_iframe_src
+      if @dialog_type == 'video'
+        @iframe_src = refinery.insert_videos_admin_videos_path url_params.merge(:dialog => true)
+      else
+        super
+      end
     end
   end
-  alias_method_chain :find_iframe_src, :video
-end
+)
