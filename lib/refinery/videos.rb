fac63655092a35e60663e02ef4df8c18a88c252a
Leon J Mac <leonjmac@protonmail.ch>
2019-10-14 23:49:45 +0100

    Update videos.rb

--- lib/refinery/videos.rb
+++ lib/refinery/videos.rb
@@ -1,28 +1,27 @@
 require 'refinerycms-core'
-require 'dragonfly'
-require 'rack/cache'
-
+require 'refinerycms-dragonfly'
 
 module Refinery
   autoload :VideosGenerator, 'generators/refinery/videos_generator'
 
   module Videos
     require 'refinery/videos/engine'
+    require 'refinery/dragonfly/extension_configuration'
     require 'refinery/videos/configuration'
-    autoload :Dragonfly, 'refinery/videos/dragonfly'
     autoload :Validators, 'refinery/videos/validators'
 
     class << self
-      attr_writer :root
-
       def root
         @root ||= Pathname.new(File.expand_path('../../../', __FILE__))
       end
 
       def factory_paths
-        @factory_paths ||= [ root.join('spec', 'factories').to_s ]
+        @factory_paths ||= [ root.join("spec/factories").to_s ]
       end
     end
   end
 end
 
+ActiveSupport.on_load(:active_record) do
+  require 'mobility'
+end
