9cf612e19c3c75acf78801d7464e3a7d2e9da6ae
Leon J Mac <leonjmac@protonmail.ch>
2019-10-15 00:04:30 +0100

    Reinstated backend routes with correct scoping.

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,24 @@
 Refinery::Core::Engine.routes.draw do
   get '/system/videos/*dragonfly', :to => Dragonfly.app(:refinery_videos)
+  
+  # Frontend routes
+  namespace :videos do
+    resources :videos, :path => '', :only => [:index, :show]
+  end
 
-  namespace :admin, :path => Refinery::Core.backend_route do
-    resources :videos, :except => :show do
-      get :insert, :on => :collection
+  # Backend routes
+  namespace :videos, :path => '' do
+    namespace :admin, :path => Refinery::Core.backend_route do
+      resources :videos do
+        post :append_to_wym
+        collection do
+          post :update_positions
+          get :insert
+          get :dialog_preview
+        end
+      end
+      resources :video_files, :only => [:destroy]
     end
   end
+
 end
-
-# Refinery::Core::Engine.routes.append do
-#   get '/system/videos/*dragonfly', to: Dragonfly.app(:refinery_videos)
-#   # Frontend routes
-#   namespace :videos do
-#     resources :videos, :path => '', :only => [:index, :show]
-#   end
-
-#   # Admin routes
-#   namespace :videos, :path => '' do
-#     namespace :admin, :path => 'refinery' do
-#       resources :videos do
-#         post :append_to_wym
-#         collection do
-#           post :update_positions
-#           get :insert
-#           get :dialog_preview
-#         end
-#       end
-#       resources :video_files, :only => [:destroy]
-#     end
-#   end
-
-# end
