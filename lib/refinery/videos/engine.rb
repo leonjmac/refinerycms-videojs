ae2574917cce129f9723e399cb43590561335671
Leon J Mac <leonjmac@protonmail.ch>
2019-10-14 18:03:05 +0100

    Update engine.rb

--- lib/refinery/videos/engine.rb
+++ lib/refinery/videos/engine.rb
@@ -2,13 +2,13 @@ module Refinery
   module Videos
     class Engine < Rails::Engine
       include Refinery::Engine
-      isolate_namespace Refinery::Videos
 
+      isolate_namespace Refinery
       engine_name :refinery_videos
 
       initializer 'attach-refinery-videos-with-dragonfly', before: :finisher_hook do |app|
-        ::Refinery::Videos::Dragonfly.configure!
-        ::Refinery::Videos::Dragonfly.attach!(app)
+        ::Refinery::Videos::Dragonfly.configure!(::Refinery::Videos)
+        ::Refinery::Videos::Dragonfly.attach!(app, ::Refinery::Videos)
       end
 
      before_inclusion do
@@ -17,7 +17,7 @@ module Refinery
           plugin.name = 'refinery_videos'
           plugin.menu_match = %r{refinery/videos/video(_dialog)?s$}
           plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.videos_admin_videos_path }
-          plugin.always_allow_access = true
+          #plugin.always_allow_access = true
         end
       end
 
